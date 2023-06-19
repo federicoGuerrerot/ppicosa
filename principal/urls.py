d5963f6c5b99ab1e04eac178d864cf7fec18adbf
federicoGuerrerot <fguerrerot@unal.edu.co>
2023-06-18 21:19:35 -0500

    PPE8

--- principal/urls.py
+++ principal/urls.py
@@ -1,6 +1,10 @@
+# Importar librerias necesarias
 from django.urls import path
+
+# Importar la vista de la aplicación principal
 from . import views
 
 urlpatterns = [
+    # Ruta de la página principal
     path('', views.home, name='home'),
 ]
--- principal/views.py
+++ principal/views.py
@@ -5,4 +5,8 @@ from django.shortcuts import render
 def home(request):
     """ Vista para la página de inicio """
 
+    # El renderizado de la página se hace en el archivo base.html
+    # en este se encuentra la estructura de la página
+    # y el codigo JS para el funcionamiento de la página
+
     return render(request, 'base.html')
